c9da495a93e647d49294a77a5f3c3814cd0dec96
Sammy <skariuki@seaburysolutions.com>
2026-01-27 11:30:36 +0300

    Initial commit

---
+++ Infrastructure/Config/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace Infrastructure.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(prop=>prop.Price).HasColumnType("decimal(18,2)");
+            builder.Property(builder => builder.Name).IsRequired();
+
+        }
+
+    }
+
+}   
